6ae8bcb9eac596c5d78aebcf88fa9775976a16ba
Joseph Noblett <noblettjoe@gmail.com>
2024-04-16 11:27:49 -0400

    image upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,18 @@ export default async function HomePage() {
 
   async function Images() {
     const images = await db.query.images.findMany({
-      orderBy:(model, { desc }) => desc(model.id),
+      orderBy: (model, { desc }) => desc(model.id),
     });
 
     return (
       <div className="flex flex-wrap gap-4">
-      {[...images].map((image) => (
-        <div key={image.id} className="w-48">
-          <img src={image.url} alt="image" />
-          <div>{image.name}</div>
-        </div> 
-      ))}
-    </div>
+        {[...images].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="image" />
+            <div>{image.name}</div>
+          </div>
+        ))}
+      </div>
     );
   }
 
